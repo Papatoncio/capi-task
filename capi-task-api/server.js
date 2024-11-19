570c056695dd1e871ebf702fe109470f547ee468
Papatoncio <juanpij23@hotmail.com>
2024-11-18 18:53:50 -0600

    Configuracion docker-compose

--- capi-task-api/server.js
+++ capi-task-api/server.js
@@ -1,17 +1,25 @@
 const express = require("express");
+const healthRoutes = require("./routes/healthRoutes");
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Permite solicitudes desde cualquier origen
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
+    allowedHeaders: ["Content-Type", "Authorization"], // Headers permitidos
+  })
+);
 
 app.use(express.json());
 
+app.use("/api/health", healthRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
